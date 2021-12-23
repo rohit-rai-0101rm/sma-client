549e1e972150857dd25febfc79af9835fdd9531e
rohit rai <rohitrai.dev@gmail.com>
2021-12-23 08:10:47 +0530

    image for pin detail fetched

--- src/components/PinDetails.jsx
+++ src/components/PinDetails.jsx
@@ -1,10 +1,56 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { MdDownloadForOffline } from 'react-icons/md';
+import { Link, useParams } from 'react-router-dom';
+import { v4 as uuidv4 } from 'uuid';
 
-const PinDetails = () => {
+import { client, urlFor } from '../client';
+import MasonryLayout from './MasonryLayout';
+import { pinDetailMorePinQuery, pinDetailQuery } from '../utils/data';
+import Spinner from './Spinner';
+
+const PinDetails = ({ user }) => {
+    const { pinId } = useParams()//only fetches dynamic parameter
+
+    const [pins, setPins] = useState(null)
+    const [pinDetail, setPinDetail] = useState(null)
+    const [comment, setComment] = useState('');
+    const [addingComment, setAddingComment] = useState(false);
+
+
+    const fetchPinDetails = () => {
+        const query = pinDetailQuery(pinId);
+
+        if (query) {
+            client.fetch(`${query}`).then((data) => {
+                setPinDetail(data[0]);
+                console.log(data);
+                if (data[0]) {
+                    const query1 = pinDetailMorePinQuery(data[0]);
+                    client.fetch(query1).then((res) => {
+                        setPins(res);
+                    });
+                }
+            });
+        }
+    };
+
+    useEffect(() => {
+        fetchPinDetails();
+    }, [pinId]);
+
+
+    if (!pinDetail) return <Spinner message="loading pin details" />
+
     return (
-        <div>
-            pin details
-        </div>
+        <div className="flex xl:flex-row flex-col m-auto bg-white" style={{ maxWidth: '1500px', borderRadius: '32px' }}>
+          <div className="flex justify-center items-center md:items-start flex-initial">
+            <img
+              className="rounded-t-3xl rounded-b-lg"
+              src={(pinDetail?.image && urlFor(pinDetail?.image).url())}
+              alt="user-post"
+            />
+          </div>
+          </div>
     )
 }
 
