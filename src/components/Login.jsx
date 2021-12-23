9886013a5aa3c475f4e981d15364a88568accde4
rohit rai <rohitrai.dev@gmail.com>
2021-12-23 18:15:08 +0530

    everything working fine

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -8,75 +8,61 @@ import shareVideo from '../assets/share.mp4'
 import logo from '../assets/logowhite.png'
 import {client} from '../client'
 const Login = () => {
-    const navigate=useNavigate()
-    const dispatch = useDispatch()
+    const navigate = useNavigate();
     const responseGoogle = (response) => {
-        console.log(response)
-        localStorage.setItem('user',JSON.stringify(response.profileObj))
-        //console.log(response)
-       const{name,googleId,imageUrl,email}=response.profileObj
-       const doc={
-           _id:googleId,
-           _type:'user',
-           userName:name,
-           image:imageUrl,
-
-       }
-       client.createIfNotExists(doc)
-       .then(()=>{
-           navigate('/',{replace:true})
-
-       })
-       dispatch(login({
-            name,
-            email,
-            googleId,
-            imageUrl
-        }))
-        navigate('/')
-
-    }
+      localStorage.setItem('user', JSON.stringify(response.profileObj));
+      const { name, googleId, imageUrl } = response.profileObj;
+      const doc = {
+        _id: googleId,
+        _type: 'user',
+        userName: name,
+        image: imageUrl,
+      };
+      client.createIfNotExists(doc).then(() => {
+        navigate('/', { replace: true });
+      });
+    };
+  
     return (
-        <div className='flex justify-start items-center flex-col h-screen'>
-            <div className='relative w-full h-full'>
-                <video
-                    src={shareVideo}
-                    type="video/mp4"
-                    loop
-                    controls={false}
-                    muted
-                    autoPlay
-                    className="w-full h-full object-cover"
-                />
-
-                <div className="absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0    bg-blackOverlay">
-                    <div className="p-5">
-                        <img src={logo} width="130px" />
-                    </div>
-                    <div className='shadow-2xl'>
-                        <GoogleLogin
-                            clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
-                            render={(renderProps) => (
-                                <button
-                                    type="button"
-                                    onClick={renderProps.onClick}
-                                    disabled={renderProps.disabled}
-                                    className='bg-mainColor flex justidy-center items-center p-3 rounded-lg cursor-pointer outline-none'>
-                                    <FcGoogle className='mr-5' />
-                                    Sign in with Google
-
-                                </button>
-                            )}
-                            onSuccess={responseGoogle}
-                            onSuccess={responseGoogle}
-                            cookiePolicy='single_host_origin'
-
-                        />
-                    </div>
-                </div>
+      <div className="flex justify-start items-center flex-col h-screen">
+        <div className=" relative w-full h-full">
+          <video
+            src={shareVideo}
+            type="video/mp4"
+            loop
+            controls={false}
+            muted
+            autoPlay
+            className="w-full h-full object-cover"
+          />
+  
+          <div className="absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0    bg-blackOverlay">
+            <div className="p-5">
+              <img src={logo} width="130px" />
             </div>
+  
+            <div className="shadow-2xl">
+              <GoogleLogin
+                clientId={`${process.env.REACT_APP_GOOGLE_API_TOKEN}`}
+                render={(renderProps) => (
+                  <button
+                    type="button"
+                    className="bg-mainColor flex justify-center items-center p-3 rounded-lg cursor-pointer outline-none"
+                    onClick={renderProps.onClick}
+                    disabled={renderProps.disabled}
+                  >
+                    <FcGoogle className="mr-4" /> Sign in with google
+                  </button>
+                )}
+                onSuccess={responseGoogle}
+                onFailure={responseGoogle}
+                cookiePolicy="single_host_origin"
+              />
+            </div>
+          </div>
         </div>
-    )
-}
-
-export default Login
+      </div>
+    );
+  };
+  
+  export default Login;
